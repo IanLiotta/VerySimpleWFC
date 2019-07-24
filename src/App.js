fe3460e63304bf8c92bc17a0dfa2c364333d45f6
Glitch (meowing-aries) <none>
2019-07-24 21:57:27 +0000

    Draw rules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
+import resultCanvas, { ruleDisplay } from './resultCanvas.js';
 import './App.css';
 
+// ****** Constants ******
 const TABLE_SIZE = 20;
 
 const colors = [
@@ -11,13 +13,7 @@ const colors = [
   'blue'
 ];
 
-const colorsRGBA = [
-  'rgba(128,128,128, 1)',
-  'rgba(255,50,50, 0.25)',
-  'rgba(50,255,50, 0.25)',
-  'rgba(50,50,255, 0.25)'
-];
-
+// ****** Buttons for the rule grid ******
 const RuleButton = ({row, col, updateRules}) => {
   const [colorIdx, setColorIdx] = useState(1);
   
@@ -41,6 +37,8 @@ const RuleButton = ({row, col, updateRules}) => {
   
 }
 
+
+// ****** The Rule Grid ******
 const RuleInput = ({size, rules, setRules}) => {
   const [ruleSource, setRuleSource] = useState([]);
 
@@ -113,7 +111,7 @@ const RuleInput = ({size, rules, setRules}) => {
     for(let cols = 0; cols < size; cols++){
       colTemp.push(<RuleButton key={cols} row={rows} col={cols} updateRules={updateRules} />)
     }
-    result.push(<div key={rows} className="buttonRow">{colTemp}</div>);
+    result.push(<div key={rows} className="row-container">{colTemp}</div>);
   }
   
   useEffect(() => {
@@ -122,83 +120,19 @@ const RuleInput = ({size, rules, setRules}) => {
     }
   });
   
-  return(result);
+  return(<div>
+      <h2>Rule Input</h2>
+      {result}
+    </div>);
 }
 
-const ResultButton = ({row, col, parseNeighbors, table, setTable}) => {
-  let [colorIdx, changeColorIdx] = useState(0);
-
-  const solveCell = () => {
-    const possible = parseNeighbors(row, col, table);
-    const newColorIdx = Math.floor(Math.random() * possible.length);
-    changeColorIdx(possible[newColorIdx]);
-    const newTable = table;
-    newTable[row][col] = possible[newColorIdx];
-    setTable(newTable);
-  }
-     
-  return(
-  <button
-    className={`resultButton ${colors[colorIdx]}`}
-    onClick={solveCell}
-    ></button>
-    )
-}
-
-const resultCanvas = ( res ) => {
-  let table = [];
-  let pinned = [];
-  res.setup = () => {
-    res.createCanvas(TABLE_SIZE*20, TABLE_SIZE*20);   
-  }
-
-  res.myCustomRedrawAccordingToNewPropsHandler = (props) => {
-    table = props.table;
-    pinned = props.pinned;
-  }
-  
-  res.drawPossible = (row, col, xPx = 20, yPx = 20) => {
-    let possible = table[row][col];
-    if(possible && !Array.isArray(possible)){
-      possible = [possible];
-    }
-    /* This is the fill function I want - commented out for performance testing
-    if(possible && possible.length > 0){
-      for(const value of possible) {
-        res.fill(colorsRGBA[value]);
-        res.rect(col*xPx, row*yPx, xPx, yPx);
-        if(pinned[row][col]){
-          res.rect(col*xPx+xPx/2, row*yPx+yPx/2, 2, 2,);
-        }
-      }
-    }
-    */
-    if(possible && possible.length === 1){
-      res.fill(colorsRGBA[possible[0]]);
-      res.rect(col*xPx, row*yPx, xPx, yPx);
-    }
-    else {
-      res.fill('magenta');
-      res.rect(col*xPx, row*yPx, xPx, yPx);
-    }
-  }
-
-  res.draw = (props) => {
-    for(let x = 0; x < table.length; x++){
-      for(let y = 0; y < table[x].length; y++){
-        res.drawPossible(x, y);
-      }
-    }
-  }
-}
-
+// ****** The output and where the algorithm lives ******
 const ResultOutput = ({rules}) => {
   const [table, setTable] = useState([]);
   const [pinned, setPinned] = useState([]);
   const [nextMoves, setNextMoves] = useState([]);
   const [loop, setLoop] = useState(false);
-  const [history, setHistory] = useState([]);
-  
+
   const createTable = ( size ) => {
     const newTable = [];
     for(let x = 0; x < size; x++){
@@ -253,6 +187,7 @@ const ResultOutput = ({rules}) => {
   const resetTable = () => {
     setLoop(false);
     setTable(createTable(TABLE_SIZE));
+    
     let newPinned = [];
       for(let x = 0; x < TABLE_SIZE; x++){
         let cols = []
@@ -262,23 +197,26 @@ const ResultOutput = ({rules}) => {
         newPinned.push(cols);
       }
     setPinned(newPinned);
+    /* Failure state testing data
+    setTable([[[3,1,2],[3,1,2],[],[],[],[3],[3],[3],[3],[3],[3],[3,1],[3],[3],[3,1],[3],[3],[3],[3],[3,1]],[[1,3,2],[],[],[],[],[3],[3],[3],[3],[3],[3],[1,3],[3],[3],[1,3],[3],[3],[3],[3],[1]],[[],[],[],[],[],[],[3],[3],[3],[3],[3],[1],[3],[3],[1],[3],[3],[3],[3],[1]],[[],[],[],[],[],[],[],[3],[3],[3],[3],[1],[1,3],[3],[1],[3],[3],[3],[3],[1]],[[],[],[],[],[],[],[],[3],[3],[3],[3],[1],[1],[1,3],[1],[3],[3],[3],[3],[1]],[[],[],[],[],[],[],[],[],[],[3],[3],[1],[1,2],[1],[1],[3],[3],[3],[3],[1]],[[],[],[],[],[],[],[1],[],[],[3],[3],[1],[2],[1],[1],[3],[3],[3],[1,3],[1]],[[],[],[],[],[],[],[1],[1],[1],[3],[3],[1],[2],[1],[1],[3],[3],[3],[1],[1,2]],[[],[],[],[],[],[],[1],[1],[1],[3],[1,3],[1],[2],[1],[1],[3],[3],[3],[1],[2]],[[],[],[],[],[],[1],[1],[1],[1],[3],[1,3],[1],[2],[1],[1],[3],[3],[3],[1],[2]],[[],[],[],[],[],[],[],[1],[1],[3],[1,3],[1],[2],[1],[1],[3],[3],[3],[1],[2]],[[],[],[],[],[],[],[],[1],[1],[3],[1],[1],[2],[1],[1],[3],[3],[3],[1],[2]],[[],[],[],[],[],[1],[2],[1],[1],[3],[1],[1],[2],[1,2],[1],[3],[3],[3],[1],[2]],[[],[],[],[],[],[],[2],[1],[1],[3],[1],[1],[2],[1,2],[1],[1,3],[3],[3],[1],[2]],[[],[],[],[],[],[],[2],[1],[1],[3],[1],[1],[2],[2],[1],[1,3],[3],[3],[1],[2]],[[],[],[],[],[],[],[2],[1,2],[1],3,[1],[1],[2],[2],[1],[1],[3],[3],[1],[2]],[[],[],[],[],[],[],[2],[1,2],[1,2],[1,3],[1],[1],[2],[2],[1],[1],[3],[3],[1],[2]],[[],[],[],[],[],[1],[2],[2],[1,2],[1],[1],[1],[2],[2],[1,2],[1],3,[3],[1],[2]],[[],[],[],[],[],[],[2],[2],[1,2],[1,2],[1,2],[1],[2],[2],[1,2],[1],[3],[1,3],[1],[2]],[[],[],[],[],[],[],[],[2],[2],[1,2],[1,2],[1,2],[2],[2],[2],[1],[1,3],[1],[1,2],[2]]]);
+    setHistory([[18,15],[18,16],[13,15],[1,14],[13,17],[10,13],[14,15],[15,17],[12,13],[7,18],[6,18],[19,16],[7,19],[16,17],[1,19],[0,19],[13,13],[5,13],[4,13],[10,12],[18,17],[18,14],[5,12],[16,14],[3,12],[18,11],[19,17],[19,18],[10,10],[19,11],[0,14],[14,10],[0,11],[19,10],[12,9],[4,11],[1,11],[5,8],[18,10],[17,14],[6,8],[5,7],[5,6],[6,7],[7,10],[19,9],[18,9],[11,7],[10,6],[15,9],[11,6],[16,9],[15,7],[8,10],[19,7],[2,6],[3,6],[14,5],[16,7],[18,5],[10,5],[8,5],[16,8],[19,8],[9,4],[12,4],[18,8],[17,4],[7,5],[17,16],[15,9]]);
+    setPinned([[false,false,false,false,false,true,true,true,true,true,true,false,true,true,false,true,true,true,true,false],[false,false,false,false,false,true,true,true,true,true,true,false,true,true,false,true,true,true,true,true],[false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,false,false,true,true,true,true,true,false,true,true,true,true,true,true,true],[false,false,false,false,false,false,false,true,true,true,true,true,true,false,true,true,true,true,true,true],[false,false,false,false,false,false,false,false,false,true,true,true,false,true,true,true,true,true,true,true],[false,false,false,false,false,false,true,false,false,true,true,true,true,true,true,true,true,true,false,true],[false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,false],[false,false,false,false,false,false,true,true,true,true,false,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,false,false,true,true,true,false,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,true,true,true,true,true,true,true,true,false,true,true,true,true,true,true],[false,false,false,false,false,false,true,true,true,true,true,true,true,false,true,false,true,true,true,true],[false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,false,true,true,true,true],[false,false,false,false,false,false,true,false,true,true,true,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,false,true,false,false,false,true,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true],[false,false,false,false,false,false,true,true,false,false,false,true,true,true,false,true,true,false,true,true],[false,false,false,false,false,false,false,true,true,false,false,false,true,true,true,true,false,true,false,true]]);
+
+    */
   }
   
   const patternStep = () => {
     let tempTable = JSON.parse(JSON.stringify(table));
-    let possibleTable = JSON.parse(JSON.stringify(table));
+    let possibleTable = createTable(TABLE_SIZE);
     let tempNextMoves = nextMoves;
-    let tempHistory = history;
-    
-    let newPinned = JSON.parse(JSON.stringify(pinned));
-      for(let x = 0; x < TABLE_SIZE; x++){
-        let cols = []
-        for(let y = 0; y < TABLE_SIZE; y++){
-          cols.push(false);
-        }
-        newPinned.push(cols);
+    let newPinned = [];
+    for(let x = 0; x < TABLE_SIZE; x++){
+      let cols = []
+      for(let y = 0; y < TABLE_SIZE; y++){
+        cols.push(pinned[x][y]);
       }
-      
+      newPinned.push(cols);
+    }
     //pick a random start position
     let row, col;
     let possible = [];
@@ -301,14 +239,14 @@ const ResultOutput = ({rules}) => {
       }
       return tempNextMoves;
     }
-    
+
     if(tempNextMoves.length === 0){
       row = Math.floor(Math.random() * TABLE_SIZE);
       col = Math.floor(Math.random() * TABLE_SIZE);
       possible = parseNeighbors(row, col, tempTable);
     }
     else {
-
+      
       while(possible.length === 0 && tempNextMoves.length > 0){
         //pick a random valid move to pin next
         const randIdx = Math.floor(Math.random() * tempNextMoves.length);
@@ -316,14 +254,7 @@ const ResultOutput = ({rules}) => {
         row = chosenMove[0];
         col = chosenMove[1];
         possible = parseNeighbors(row, col, tempTable);
-        if(possible.length === 0) {
-          console.count('backtracking!');
-          let [prevRow, prevCol] = tempHistory.length-1;
-          tempTable[prevRow][prevCol] = parseNeighbors(prevRow, prevCol, tempTable);
-          newPinned[prevRow][prevCol] = false;
-          tempHistory.shift();
-          tempNextMoves = findNextMoves();
-        }
+        tempNextMoves = tempNextMoves.slice(0, randIdx).concat(tempNextMoves.slice(randIdx+1));
       }
     }
     
@@ -332,7 +263,6 @@ const ResultOutput = ({rules}) => {
       const newColorIdx = Math.floor(Math.random() * possible.length);
       tempTable[row][col] = possible[newColorIdx];
       newPinned[row][col] = true;
-      
       //evaluate what possibilities our neighbors have left.
       let newPins = false;
       do{
@@ -358,9 +288,7 @@ const ResultOutput = ({rules}) => {
       //add adjacent cells to the list of potential next moves.
       tempNextMoves = findNextMoves();
     }    
-    
     //done with step; set our state to the result.
-    setHistory(history.concat([row, col]));
     setTable(tempTable);
     setPinned(newPinned);
     setNextMoves(tempNextMoves);
@@ -372,7 +300,6 @@ const ResultOutput = ({rules}) => {
   }
   
   useEffect(() => {
-    let nextStep;
     if(table.length === 0){
       setTable(createTable(TABLE_SIZE));
       let newTable = [];
@@ -393,15 +320,6 @@ const ResultOutput = ({rules}) => {
     }
     
   });
-  
-  let result = []
-  for(let rows = 0; rows < TABLE_SIZE; rows++){
-    let colTemp = []
-    for(let cols = 0; cols < TABLE_SIZE; cols++){
-      colTemp.push(<ResultButton key={cols} row={rows} col={cols} parseNeighbors={parseNeighbors} table={table} setTable={setTable} />)
-    }
-    result.push(<div key={rows} className="buttonRow">{colTemp}</div>);
-  }
 
   return(
     <div>
@@ -419,16 +337,19 @@ const ResultOutput = ({rules}) => {
     );
 }
 
+
+// ******* The Main App ******
 const App = () => {
   
   const [rules, setRules] = useState([]);
-  
+  //let debugRules = [{"source":3,"dest":1,"dir":"d"},{"source":3,"dest":3,"dir":"r"},{"source":3,"dest":3,"dir":"d"},{"source":3,"dest":3,"dir":"l"},{"source":1,"dest":3,"dir":"u"},{"source":1,"dest":1,"dir":"d"},{"source":1,"dest":3,"dir":"r"},{"source":3,"dest":3,"dir":"u"},{"source":3,"dest":1,"dir":"l"},{"source":3,"dest":1,"dir":"r"},{"source":1,"dest":2,"dir":"d"},{"source":1,"dest":3,"dir":"l"},{"source":1,"dest":1,"dir":"r"},{"source":1,"dest":1,"dir":"l"},{"source":1,"dest":1,"dir":"u"},{"source":1,"dest":2,"dir":"r"},{"source":2,"dest":1,"dir":"u"},{"source":2,"dest":2,"dir":"d"},{"source":2,"dest":1,"dir":"l"},{"source":2,"dest":1,"dir":"r"},{"source":1,"dest":2,"dir":"l"},{"source":2,"dest":2,"dir":"r"},{"source":2,"dest":2,"dir":"l"},{"source":2,"dest":2,"dir":"u"}];
   return (
     <div className="App">
-      <h2>Rules</h2>
-      <RuleInput size={4} rules={rules} setRules={setRules} />
-      <h2>Result</h2>
-      <ResultOutput rules={rules}/>
+      <div className="column-container">
+        <RuleInput size={4} rules={rules} setRules={setRules} />
+        <ResultOutput rules={rules}/>
+      </div>
+      <P5Wrapper sketch={ruleDisplay} rules={rules}/>
     </div>
   );
 }
